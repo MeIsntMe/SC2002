fda9aaf26cd6adecd0d63534656f6b6998dc5d61
Gracelynn <gracelynn13@gmail.com>
2024-11-05 22:11:59 +0800

    Menu basic structure

--- Menu/Menu.java
+++ Menu/Menu.java
@@ -1,5 +1,21 @@
 package Menu;
 
+import User.User;
+import java.util.ArrayList;
+
 public class Menu {
-    
+    public static void displayOptions(User user){
+        System.err.println("Methods: ");
+        int i = 1;
+        for (String option:user.getAvailableMethods()){
+            System.err.printf("%d: %s", i, option);
+            i += 1;
+        }
+    }
+
+    public static void output(ArrayList<String> information){
+        for (String line:information){
+            System.out.println(line);
+        }
+    }
 }
