89306db6e77a70422e93b27b6a80f7d6f5a663fe
SSS963 <88152437+SSS963@users.noreply.github.com>
2024-11-11 22:56:38 +0800

    Update Medicine.java

--- hospitalsystem/model/Medicine.java
+++ hospitalsystem/model/Medicine.java
@@ -1,24 +1,54 @@
 package hospitalsystem.model;
 
+import java.time.LocalDate;
+
 public class Medicine {
     private String medicineName;
     private int initialStock;
     private int lowStockAlert;
+    private LocalDate expirationDate;
 
-    public Medicine(String medicineName, int initialStock, int lowStockAlert) {
+    // Constructor
+    public Medicine(String medicineName, int initialStock, int lowStockAlert, LocalDate expirationDate) {
         this.medicineName = medicineName;
         this.initialStock = initialStock;
         this.lowStockAlert = lowStockAlert;
+        this.expirationDate = expirationDate;
     }
 
-    public String getMedicineName() { return medicineName; }
-    public int getInitialStock() { return initialStock; }
-    public int getLowStockAlert() { return lowStockAlert; }
-
-    public void setInitialStock(int newStockLevel) {
-        this.initialStock = newStockLevel;
+    // Getters
+    public String getMedicineName() {
+        return medicineName;
     }
-    public void setLowStockAlert(int newAlert) {
-        this.lowStockAlert = newAlert;
+
+    public int getInitialStock() {
+        return initialStock;
+    }
+
+    public int getLowStockAlert() {
+        return lowStockAlert;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    // Setters
+    public void setInitialStock(int initialStock) {
+        this.initialStock = initialStock;
+    }
+
+    public void setLowStockAlert(int lowStockAlert) {
+        this.lowStockAlert = lowStockAlert;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    // Method to check if medication is nearing expiration (within 2 weeks)
+    public boolean isNearingExpiration() {
+        LocalDate today = LocalDate.now();
+        return expirationDate.isBefore(today.plusWeeks(2));
     }
 }
