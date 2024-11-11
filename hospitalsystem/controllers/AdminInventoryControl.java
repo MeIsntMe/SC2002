800edfc598e9aa184420fbcea7101b6660730187
SSS963 <88152437+SSS963@users.noreply.github.com>
2024-11-11 22:05:12 +0800

    Update AdminInventoryControl.java

--- hospitalsystem/controllers/AdminInventoryControl.java
+++ hospitalsystem/controllers/AdminInventoryControl.java
@@ -1,1 +1,27 @@
+package hospitalsystem.controllers;
+
+import java.util.Scanner;
+
+public class AdminInventoryControl extends InventoryControl {
+
+    public AdminInventoryControl() {
+        super();
+    }
+
+    // Method specific to Administrator to update the low stock alert line
+    public void updateLowStockAlert(Scanner sc) {
+        System.out.print("Enter the name of the medicine to update low stock alert: ");
+        String medicineName = sc.nextLine().trim();
+        if (inventoryMap.containsKey(medicineName)) {
+            Inventory medicine = inventoryMap.get(medicineName);
+            System.out.print("Enter the new low stock alert level: ");
+            int newAlertLine = sc.nextInt();
+            sc.nextLine();
+            medicine.setLowStockAlert(newAlertLine);
+            System.out.println("Low stock alert level for " + medicineName + " updated to " + newAlertLine);
+        } else {
+            System.out.println("Medicine not found in inventory: " + medicineName);
+        }
+    }
+}
 
