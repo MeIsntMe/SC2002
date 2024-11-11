d8e9c2258e400a9a002507a98ab898f69a1b1db9
SSS963 <88152437+SSS963@users.noreply.github.com>
2024-11-11 22:03:09 +0800

    Update PharmacistInventoryControl.java

--- hospitalsystem/controllers/PharmacistInventoryControl.java
+++ hospitalsystem/controllers/PharmacistInventoryControl.java
@@ -1,1 +1,27 @@
+package hospitalsystem.controllers;
+
+import java.util.Scanner;
+
+public class PharmacistInventoryControl extends InventoryControl {
+
+    public PharmacistInventoryControl() {
+        super();
+    }
+
+    // Method specific to Pharmacist to submit a replenishment request
+    public void submitReplenishmentRequest(Scanner sc) {
+        System.out.print("Enter the medication name to request replenishment: ");
+        String medicationName = sc.nextLine();
+        if (inventoryMap.containsKey(medicationName)) {
+            System.out.print("Enter the quantity for replenishment: ");
+            int quantity = sc.nextInt();
+            sc.nextLine();
+            Inventory medicine = inventoryMap.get(medicationName);
+            medicine.setInitialStock(medicine.getInitialStock() + quantity);
+            System.out.println("Replenishment request submitted for " + medicationName + ", added " + quantity + " units.");
+        } else {
+            System.out.println("Error: Medication " + medicationName + " not found in inventory.");
+        }
+    }
+}
 
