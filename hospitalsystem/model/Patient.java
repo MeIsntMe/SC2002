449b667dbea19a40b86169fcd4b7315c28525442
Gracelynn <gracelynn13@gmail.com>
2024-11-13 20:02:30 +0800

    more fix

--- hospitalsystem/model/Patient.java
+++ hospitalsystem/model/Patient.java
@@ -3,24 +3,17 @@ import hospitalsystem.controllers.*;
 import java.util.List;
 
 public class Patient extends User{
-    private final String patientID;
     private final MedicalRecord medicalRecord;
     private List<Appointment> appointments;
 
     public Patient(String HospitalID, String name, String gender, String password){
         super(HospitalID, name, gender, password);
-        this.patientID = HospitalID;
         this.medicalRecord = new MedicalRecord(this);
         this.appointments = AppointmentControl.getAppointmentsByPatientID(this.patientID);
     }
 
     public MedicalRecord getMedicalRecord(){
         return this.medicalRecord;
-    }
-
-    @Override
-    public String getID(){
-        return this.patientID;
     }
 
     public List<Appointment> getAppointments(){
