6f467cdc5c59aa9fe57e6c83c281cc9fdc983423
SSS963 <88152437+SSS963@users.noreply.github.com>
2024-11-16 20:35:45 +0800

    Update ReplenishmentRequest.java

--- hospitalsystem/model/ReplenishmentRequest.java
+++ hospitalsystem/model/ReplenishmentRequest.java
@@ -3,27 +3,26 @@ package hospitalsystem.model;
 import hospitalsystem.enums.RequestStatus;
 
 public class ReplenishmentRequest {
-    private final int requestID; 
-    private final String medicineName;
+    private final int requestID;
+    private final Medicine medicine; // Changed from String to Medicine
     private final int requestedQuantity;
     private RequestStatus status;
 
-    // Constructor with expiration date
-    public ReplenishmentRequest(int requestID, String medicineName, int requestedQuantity) {
+    // Constructor
+    public ReplenishmentRequest(int requestID, Medicine medicine, int requestedQuantity) {
         this.requestID = requestID;
-        this.medicineName = medicineName;
+        this.medicine = medicine; // Store the Medicine object
         this.requestedQuantity = requestedQuantity;
         this.status = RequestStatus.PENDING; // Default status to pending approval
     }
 
     // Getters and setters
-
-    public int getRequestID(){
+    public int getRequestID() {
         return requestID;
     }
 
-    public String getMedicineName() {
-        return medicineName;
+    public Medicine getMedicine() {
+        return medicine; // Return the Medicine object
     }
 
     public int getRequestedQuantity() {
@@ -36,5 +35,15 @@ public class ReplenishmentRequest {
 
     public void setStatus(RequestStatus newStatus) {
         this.status = newStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "ReplenishmentRequest{" +
+                "requestID=" + requestID +
+                ", medicine=" + medicine.getMedicineName() + // Use Medicine object to get its name
+                ", requestedQuantity=" + requestedQuantity +
+                ", status=" + status +
+                '}';
     }
 }
